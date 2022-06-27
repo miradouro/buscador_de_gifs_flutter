8c906bc3ccbb8e8306a02d0ad9a2403fc73b708e
miradouro <rafaelmiradouro@gmail.com>
2022-06-27 15:18:39 -0300

    segundo commit

---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:buscador_gifs/ui/home_page.dart';
+
+void main() {
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: HomePage(),
+    theme: ThemeData(hintColor: Colors.white),
+  ));
+}
+
+
